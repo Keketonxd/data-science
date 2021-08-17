ac569fbb7c65372c1d7d8f3639775322171f76e1
Arkasha <ar246@yandex.ru>
2021-08-17 19:15:32 +0300

    debugged

--- HW6/bookparser/middlewares.py
+++ HW6/bookparser/middlewares.py
@@ -9,7 +9,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 
 
-class BookpasrerSpiderMiddleware:
+class BookparserSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -56,7 +56,7 @@ class BookpasrerSpiderMiddleware:
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class BookpasrerDownloaderMiddleware:
+class BookparserDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
